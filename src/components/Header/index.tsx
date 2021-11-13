f140e28f3a001b3bf65f2804f65643118228d41c
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-13 19:16:37 -0300

    ✨ Criando componente header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+export const Header: React.FC = () => {
+  return (
+    <Container>
+      <h1>Meme Generator</h1>
+    </Container>
+  )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  width: 100%;
+  height: fit-content;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: beige;
+
+  h1 {
+    margin: 0;
+    padding: 15px;
+
+    font-size: 25px;
+  }
+`
