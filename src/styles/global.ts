dee6cc5f477e2a19bb5e8d7ad33431ca79cf9012
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-13 19:31:19 -0300

    💄 Criando variáveis para cores

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,18 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const Global = createGlobalStyle`
+  :root {
+    --backgroundColor: #181818;
+    --darkerColor: #151515;
+    --textColor: #f0f0f0;
+  }
+
+  body {
+    color: var(--textColor);
+    background-color: var(--backgroundColor);
+    font-family: 'Anton', sans-serif;
+  }
+
   * {
     margin: 0;
     padding: 0;
