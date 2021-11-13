8dce7a1291dcc36521fcedd06e8435c029198fd9
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-13 18:33:34 -0300

    ✨ Usando estilização global

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom'
 
 import { App } from './App'
 
+import { Global } from './styles/global'
+
 ReactDOM.render(
   <React.StrictMode>
+    <Global />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
