444e3e87f853eab63da8cb392f5cd54e27684023
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-13 19:20:48 -0300

    ✨ Usando header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import { Header } from './components/Header'
+
 export const App: React.FC = () => {
   return (
     <div className="App">
-      Olá, mundo!
+      <Header />
     </div>
   )
 }
