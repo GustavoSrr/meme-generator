4cc27e8ffac30f00df95c4c425454b607b70b6eb
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-17 18:42:41 -0300

    ✨ Usando componente meme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import { Header } from './components/Header'
+import { Meme } from './components/Meme'
 
 export const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   )
 }
