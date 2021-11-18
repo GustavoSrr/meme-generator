fa562aa3c4d689e748b7e337e95b2d3e6af6d78a
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-18 16:50:03 -0300

    ✨ Adicionando mais inputs para texto

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,25 +1,48 @@
-import React, { FormEvent, useState } from 'react'
+import React, { FormEvent, useContext, useState } from 'react'
+import { memeContext } from '../../contexts/meme'
 
 import { Button } from '../Button'
 import { Container } from './styles'
 
 export const Input: React.FC = () => {
+  const { setMeme } = useContext(memeContext)
+
   const [url, setUrl] = useState('')
+  const [topText, setTopText] = useState('')
+  const [bottomText, setBottomText] = useState('')
 
   function handleOnSubmitForm (event: FormEvent) {
     event.preventDefault()
 
-    if (!url) return
-    console.log(url)
+    if (!url || !topText || !bottomText) return
+
+    setMeme({
+      url,
+      topText,
+      bottomText
+    })
   }
 
   return (
     <Container>
       <form onSubmit={event => handleOnSubmitForm(event)}>
         <input
-          type="text"
+          type="url"
+          required
           placeholder="Url da imagem"
           onChange={event => setUrl(event.target.value)}
+        />
+        <input
+          type="text"
+          required
+          placeholder="Texto de cima"
+          onChange={event => setTopText(event.target.value)}
+        />
+        <input
+          type="text"
+          required
+          placeholder="Texto de baixo"
+          onChange={event => setBottomText(event.target.value)}
         />
         <Button>Enviar</Button>
       </form>
