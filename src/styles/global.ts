157b11268fad8d33bb1c0a8daf98130e8fb7e797
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-18 11:44:01 -0300

    💄 Modificando e criando variáveis globais

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,8 +3,10 @@ import { createGlobalStyle } from 'styled-components'
 export const Global = createGlobalStyle`
   :root {
     --backgroundColor: #181818;
-    --darkerColor: #151515;
+    --darkerColor: #131313;
+    --darkerColorHover: #101010;
     --textColor: #f0f0f0;
+    --buttonTextColor: #d0d0d0;
   }
 
   body {
