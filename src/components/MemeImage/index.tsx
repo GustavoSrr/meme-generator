75c8d7b8ee814386bf83734b5461d6a3a320d84b
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-18 16:50:45 -0300

    ✨ Escrevendo o texto inserido nos inputs

--- src/components/MemeImage/index.tsx
+++ src/components/MemeImage/index.tsx
@@ -1,36 +1,47 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { memeContext } from '../../contexts/meme'
 
 import { createCanvas, loadImage } from 'canvas'
 
-import { Container } from './styles'
 import img from '../../assets/img.jpg'
 
+import { Container } from './styles'
+
 export const MemeImage: React.FC = () => {
+  const { meme } = useContext(memeContext)
   const [imgUrl, setImgUrl] = useState('')
 
   useEffect(() => {
+    console.log(meme)
     const imageElement = document.getElementById('memeImg')
 
-    if (imageElement) {
+    if (imageElement && meme?.topText && meme?.bottomText && meme.url) {
       const canvas = createCanvas(imageElement.clientWidth, imageElement.clientHeight)
       const ctx = canvas.getContext('2d')
 
-      ctx.font = '30px Roboto'
-      ctx.textAlign = 'center'
-      ctx.fillRect(
-        (imageElement.clientWidth - 300) / 2,
-        (imageElement.clientHeight - 300) / 2,
-        300,
-        300
-      )
+      ctx.fillRect(0, 0, 300, 300)
 
       loadImage(img).then((image) => {
         ctx.drawImage(image, 0, 0, 300, 300)
 
+        ctx.font = '25px Anton'
+        ctx.textAlign = 'center'
+        ctx.fillStyle = 'white'
+
+        ctx.fillText(meme.topText.toUpperCase(),
+          Math.round(canvas.width / 2),
+          30
+        )
+
+        ctx.fillText(meme.bottomText.toUpperCase(),
+          Math.round(canvas.width / 2),
+          290
+        )
+
         setImgUrl(canvas.toDataURL())
       })
     }
-  }, [])
+  }, [meme])
 
   return (
     <Container>
