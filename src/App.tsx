f5a049aefda836f75208c0e9e4f92a1e0be8b5b8
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-18 11:44:51 -0300

    ✨ Usando componente input

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import { Header } from './components/Header'
 import { MemeImage } from './components/MemeImage'
+import { Input } from './components/Input'
 
 export const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
       <MemeImage />
+      <Input />
     </div>
   )
 }
