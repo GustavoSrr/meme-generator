8c248d10f009025223d68dd3617b1c5d847dfc6a
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-18 12:47:40 -0300

    ✨ Usando context

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import { Header } from './components/Header'
 import { MemeImage } from './components/MemeImage'
 import { Input } from './components/Input'
 
+import { UrlProvider } from './contexts/url'
+
 export const App: React.FC = () => {
   return (
-    <div className="App">
-      <Header />
-      <MemeImage />
-      <Input />
-    </div>
+    <UrlProvider>
+      <div className="App">
+        <Header />
+        <MemeImage />
+        <Input />
+      </div>
+    </UrlProvider>
   )
 }
