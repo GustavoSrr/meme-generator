37c0bd6313634b71e8da5bd09371578db0c3a0b8
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-19 12:13:08 -0300

    ✨ Criando função para criar meme

--- src/contexts/meme.tsx
+++ src/contexts/meme.tsx
@@ -1,5 +1,7 @@
 import React, { createContext, ReactNode, useState } from 'react'
 
+import { createCanvas } from 'canvas'
+
 type memeStateType = {
   url: string;
   topText: string;
@@ -9,6 +11,7 @@ type memeStateType = {
 type memeContextType = {
   meme: memeStateType | null;
   setMeme: React.Dispatch<React.SetStateAction<memeStateType>>;
+  createMeme: () => { image: HTMLImageElement; imageUrl: string; };
 }
 
 type memeProviderProps = {
@@ -24,8 +27,49 @@ export function MemeProvider (props: memeProviderProps) {
     bottomText: 'Generator'
   })
 
+  function createMeme () {
+    const [imageUrl, setImageUrl] = useState('')
+
+    const image = new Image()
+
+    if (meme) {
+      image.src = meme.url
+
+      image.crossOrigin = 'anonymous'
+
+      const canvas = createCanvas(image.width, image.height)
+      const ctx = canvas.getContext('2d')
+
+      ctx.fillRect(0, 0, image.width, image.height)
+
+      image.onload = function () {
+        ctx.drawImage(image, 0, 0, image.width, image.height)
+
+        ctx.font = '25px Anton'
+        ctx.textAlign = 'center'
+        ctx.fillStyle = 'white'
+
+        ctx.fillText(meme.topText.toUpperCase(),
+          Math.round(image.width / 2),
+          30
+        )
+        ctx.fillText(meme.bottomText.toUpperCase(),
+          Math.round(image.width / 2),
+          image.height - 10
+        )
+
+        setImageUrl(canvas.toDataURL())
+      }
+    }
+
+    return ({
+      image: image,
+      imageUrl: imageUrl
+    })
+  }
+
   return (
-    <memeContext.Provider value={{ meme, setMeme }}>
+    <memeContext.Provider value={{ meme, setMeme, createMeme }}>
       {props.children}
     </memeContext.Provider>
   )
