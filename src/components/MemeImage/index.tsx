fdcbcd0f380294fcd8b59b5ae66720fdb22f0e07
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-19 12:14:02 -0300

    ✨ Usando função createMeme

--- src/components/MemeImage/index.tsx
+++ src/components/MemeImage/index.tsx
@@ -1,57 +1,23 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { memeContext } from '../../contexts/meme'
-
-import { createCanvas } from 'canvas'
 
 import { Container } from './styles'
 
 export const MemeImage: React.FC = () => {
-  const { meme } = useContext(memeContext)
-  const [imgUrl, setImgUrl] = useState('')
+  const { createMeme } = useContext(memeContext)
+
+  const { image, imageUrl } = createMeme()
 
   useEffect(() => {
-    const image = new Image()
-
     const imageElement = document.getElementById('memeImg')
 
-    if (imageElement && meme) {
-      image.src = meme.url
-
-      image.crossOrigin = 'anonymous'
-
-      imageElement.style.width = `${image.width}px`
-      imageElement.style.height = `${image.height}px`
-
-      const canvas = createCanvas(image.width, image.height)
-      const ctx = canvas.getContext('2d')
-
-      ctx.fillRect(0, 0, image.width, image.height)
-
-      image.onload = function () {
-        ctx.drawImage(image, 0, 0, image.width, image.height)
-
-        ctx.font = '25px Anton'
-        ctx.textAlign = 'center'
-        ctx.fillStyle = 'white'
-
-        ctx.fillText(meme.topText.toUpperCase(),
-          Math.round(image.width / 2),
-          30
-        )
-
-        ctx.fillText(meme.bottomText.toUpperCase(),
-          Math.round(image.width / 2),
-          image.height - 10
-        )
-
-        setImgUrl(canvas.toDataURL())
-      }
-    }
-  }, [0, meme])
+    imageElement!.style.width = `${image.width}px`
+    imageElement!.style.height = `${image.height}px`
+  }, [0, image])
 
   return (
     <Container>
-      <img src={imgUrl} alt="img" id="memeImg"/>
+      <img src={imageUrl} alt="img" id="memeImg"/>
     </Container>
   )
 }
