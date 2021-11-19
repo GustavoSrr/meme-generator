fa1b2224f65a36cc94fb9b91988b7c38daca6f6b
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-19 19:30:03 -0300

    ✨ Usando react-colorful para seleção de cor para o texto

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,7 +4,9 @@ import { memeContext } from '../../contexts/meme'
 import { Button } from '../Button'
 
 import { FaRegSave } from 'react-icons/fa'
-import { Container, ButtonDiv } from './styles'
+import { HexColorPicker } from 'react-colorful'
+
+import { Container, ButtonDiv, ColorPickerDiv } from './styles'
 
 export const Input: React.FC = () => {
   const { meme, setMeme, createMeme } = useContext(memeContext)
@@ -13,6 +15,7 @@ export const Input: React.FC = () => {
   const [url, setUrl] = useState(meme?.url)
   const [topText, setTopText] = useState(meme?.topText)
   const [bottomText, setBottomText] = useState(meme?.bottomText)
+  const [textColor, setTextColor] = useState(meme!.textColor)
 
   function handleOnSubmitForm (event: FormEvent) {
     event.preventDefault()
@@ -22,52 +25,58 @@ export const Input: React.FC = () => {
     setMeme({
       url,
       topText,
-      bottomText
+      bottomText,
+      textColor
     })
   }
 
   return (
-    <Container>
-      <form onSubmit={event => handleOnSubmitForm(event)}>
-        <input
-          type="url"
-          required
-          placeholder="Url da imagem"
-          onChange={event => setUrl(event.target.value)}
-          value={url}
-        />
-        <input
-          type="text"
-          required
-          placeholder="Texto de cima"
-          onChange={event => setTopText(event.target.value)}
-          value={topText}
-        />
-        <input
-          type="text"
-          required
-          placeholder="Texto de baixo"
-          onChange={event => setBottomText(event.target.value)}
-          value={bottomText}
-        />
-        <ButtonDiv>
-          <Button
-            type="submit"
-            id="SendButton"
-          >
-            Enviar
-          </Button>
-          <a href={imageUrl} download="meme.png">
+    <>
+      <Container>
+        <form onSubmit={event => handleOnSubmitForm(event)}>
+          <input
+            type="url"
+            required
+            placeholder="Url da imagem"
+            onChange={event => setUrl(event.target.value)}
+            value={url}
+          />
+          <input
+            type="text"
+            required
+            placeholder="Texto de cima"
+            onChange={event => setTopText(event.target.value)}
+            value={topText}
+          />
+          <input
+            type="text"
+            required
+            placeholder="Texto de baixo"
+            onChange={event => setBottomText(event.target.value)}
+            value={bottomText}
+          />
+          <ButtonDiv>
             <Button
-              type="button"
-              title="Baixar"
-              id="DownloadButton"
+              type="submit"
+              id="SendButton"
             >
-              <FaRegSave size={15} />
+              Enviar
             </Button>
-          </a>
-        </ButtonDiv>
-      </form>
-    </Container>
+            <a href={imageUrl} download="meme.png">
+              <Button
+                type="button"
+                title="Baixar"
+                id="DownloadButton"
+              >
+                <FaRegSave size={15} />
+              </Button>
+            </a>
+          </ButtonDiv>
+        </form>
+      </Container>
+      <ColorPickerDiv>
+        <HexColorPicker color={textColor} onChange={setTextColor} />
+      </ColorPickerDiv>
+    </>
   )
 }
