74ff1ee99cd6047779c766ec216d431f583ac0c0
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-19 19:41:36 -0300

    📱 Melhorando a responsividade

--- src/components/MemeImage/styles.ts
+++ src/components/MemeImage/styles.ts
@@ -26,4 +26,17 @@ export const ItemsDiv = styled.div`
   justify-content: center;
 
   gap: 30px;
+
+  @media only screen and (max-width: 1020px) {
+    flex-direction: column;
+    gap: 25px;
+  }
+
+  @media only screen and (max-width: 700px) {
+    width: 80%;
+  }
+
+  @media only screen and (max-width: 450px) {
+    width: 100%;
+  }
 `
