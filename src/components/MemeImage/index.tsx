27b97db435d8c9311cfa0e7f827fa54bba379a5b
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-19 11:33:03 -0300

    ♿️ Alterando tamanho da imagem de acordo com a url

--- src/components/MemeImage/index.tsx
+++ src/components/MemeImage/index.tsx
@@ -10,45 +10,48 @@ export const MemeImage: React.FC = () => {
   const [imgUrl, setImgUrl] = useState('')
 
   useEffect(() => {
+    const image = new Image()
+
     const imageElement = document.getElementById('memeImg')
 
     if (imageElement && meme) {
-      const canvas = createCanvas(imageElement.clientWidth, imageElement.clientHeight)
+      image.src = meme.url
+
+      image.crossOrigin = 'anonymous'
+
+      imageElement.style.width = `${image.width}px`
+      imageElement.style.height = `${image.height}px`
+
+      const canvas = createCanvas(image.width, image.height)
       const ctx = canvas.getContext('2d')
 
-      ctx.fillRect(0, 0, 300, 300)
-
-      const image = new Image()
-
-      image.crossOrigin = '*'
+      ctx.fillRect(0, 0, image.width, image.height)
 
       image.onload = function () {
-        ctx.drawImage(image, 0, 0, 300, 300)
+        ctx.drawImage(image, 0, 0, image.width, image.height)
 
         ctx.font = '25px Anton'
         ctx.textAlign = 'center'
         ctx.fillStyle = 'white'
 
         ctx.fillText(meme.topText.toUpperCase(),
-          Math.round(canvas.width / 2),
+          Math.round(image.width / 2),
           30
         )
 
         ctx.fillText(meme.bottomText.toUpperCase(),
-          Math.round(canvas.width / 2),
-          290
+          Math.round(image.width / 2),
+          image.height - 10
         )
 
         setImgUrl(canvas.toDataURL())
       }
-
-      image.src = meme.url
     }
   }, [0, meme])
 
   return (
     <Container>
-      <img src={imgUrl} alt="img" id="memeImg" />
+      <img src={imgUrl} alt="img" id="memeImg"/>
     </Container>
   )
 }
